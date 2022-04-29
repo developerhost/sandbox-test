49eb8b0caeea6484b4688e90495e6bb175543e8b
dirtyman <50170789+dirtyman69@users.noreply.github.com>
2022-04-29 20:44:19 +0900

    allow function

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
-import "./styles.css";
+const func1 = function (str) {
+  return str;
+};
 
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel 
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>
-`;
+console.log(func1("func1"));
+
+const func2 = (str) => {
+  return str;
+};
+
+console.log(func2("re"));
